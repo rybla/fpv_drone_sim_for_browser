cc4c0dd20fe983b7b95d65c4e4ff03b1cab92145
AD <ditzelalex@gmail.com>
2025-06-03 20:09:09 -0400

    Spec fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import RAPIER from "@dimforge/rapier3d-compat";
 import Level from "./level/Level";
+import type { Spec } from "./spec";
 
 async function main() {
   await RAPIER.init();
-  const level = new Level();
+  const spec: Spec = {
+    name: "",
+    description: "",
+    environmentId: "",
+    environmentTemperature: 75,
+    pingDelay: Math.random() * 50 + 50,
+    windEnabled: false,
+    lightingLevel: 0,
+    checkpoint_locationIds: [],
+    startpoint_locationId: "",
+    endpoint_locationId: "",
+    objects: [],
+  };
+  const level = new Level(spec);
   await level.initialize();
   level.start();
 }
